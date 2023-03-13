df074bf50c04425e783b37d741bf16d522c9707d
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-03-13 11:19:13 +0100

    fixed

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,19 +1,16 @@
 package com.epam.mjc.collections.list;
-
 import java.util.LinkedList;
 import java.util.List;
-
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int i = 0; i < sourceList.size(); i++) {
-            int num = sourceList.get(i);
-            if ((i + 1) % 2 == 0) {
-                list.addLast(num);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                linkedList.addLast(num);
             } else {
-                list.addFirst(num);
+                linkedList.addFirst(num);
             }
         }
-        return list;
+        return linkedList;
     }
 }
